57bc9240117ee87fd8cda319cac91b3a41bdf8fa
Gabriel Sene <sene300@gmail.com>
2022-10-26 19:51:29 -0300

    ArrayList

---
+++ src/br/com/brinks/teste/TesteNotas2.java
@@ -0,0 +1,25 @@
+package br.com.brinks.teste;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class TesteNotas2 {
+
+	public static void main(String[] args) {
+		ArrayList lista = new ArrayList();
+		ArrayList listaNome = new ArrayList();
+		Scanner leia = new Scanner(System.in);
+		
+		String nome = "joao", nome2 = "joaquim";
+		
+	
+		
+		
+		
+
+		
+		System.out.println(lista.get(0));
+
+	}
+
+}
